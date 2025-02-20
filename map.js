72b2b93cbbb6004ed23c1c3e30caa0eba02ebcb9
ReemaS03 <ralsaeed@ucsd.edu>
2025-02-20 09:12:10 -0800

    style of map

--- map.js
+++ map.js
@@ -4,7 +4,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoicmVlbWFzMDMiLCJhIjoiY203ZGtjYTIwMDN5ejJtcTN0Y
 // Initialize the map
 const map = new mapboxgl.Map({
     container: 'map', // ID of the div where the map will render
-    style: 'mapbox://styles/mapbox/streets-v12', // Map style
+    style: 'mapbox://styles/reemas03/cm7dkxmdy002e01sg434cbq55', // Map style
     center: [-71.09415, 42.36027], // [longitude, latitude]
     zoom: 12, // Initial zoom level
     minZoom: 5, // Minimum allowed zoom
